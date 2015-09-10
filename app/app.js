d0140c5d4d917589a0c36464bc88a4c2a5632b66
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-10 17:19:30 -0600

    Setting up main structure

--- app/app.js
+++ app/app.js
@@ -1,5 +1,25 @@
-var app = angular.module('directivePractice', []);
+/* global angular */
+var app = angular.module('directivePractice', ['ngRoute']);
 
-app.config(function(){
-
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'app/home/homeTemplate.html',
+			controller: function() {
+				var vm = this;
+				
+				vm.name = 'this';
+			},
+			controllerAs: 'vm'
+		})
+		.when('/extra-page',{
+			templateUrl: 'app/extraPage/extraPage.html',
+			controller: function() {
+				var vm = this;
+				
+				vm.that = 'that';
+			},
+			controllerAs: 'vm'
+		})
+		.otherwise({redirectTo: '/'});
 });
