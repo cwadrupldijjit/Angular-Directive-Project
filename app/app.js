f9c2bdd83de8b6d70fb7f993c957602d12387cb0
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-11 17:38:43 -0600

    Updating this as much as possible.  Custom directive and routes seem to be conflicting.

--- app/app.js
+++ app/app.js
@@ -5,21 +5,77 @@ app.config(function($routeProvider){
 	$routeProvider
 		.when('/', {
 			templateUrl: 'app/home/homeTemplate.html',
-			controller: function() {
-				var vm = this;
+			// controller: function() {
+			// 	var vm = this;
 				
-				vm.name = 'this';
-			},
-			controllerAs: 'vm'
+			// 	vm.name = 'this';
+			// },
+			// controllerAs: 'vm'
 		})
 		.when('/extra-page',{
 			templateUrl: 'app/extraPage/extraPage.html',
-			controller: function() {
-				var vm = this;
+			// controller: function() {
+			// 	var vm = this;
 				
-				vm.that = 'that';
-			},
-			controllerAs: 'vm'
+			// 	vm.that = 'that';
+			// },
+			// controllerAs: 'vm'
 		})
 		.otherwise({redirectTo: '/'});
 });
+
+app.directive('dirDisplay', [function() {
+	return {
+		restrict: 'E',
+		
+		// replace: true,
+		
+		templateUrl: 'app/home/dir-display.html',
+		
+		controller: function($scope) {
+			var vm = this;
+			
+			$scope.users = [
+				{
+					name: 'Geoff McMammy',
+					age: 43,
+					email: 'geofdude@gmail.com'
+				},
+				{
+					name: 'Samuel Skeen',
+					age: 23,
+					email: 'rndmbjnkf@gmail.com'
+				},
+				{
+					name: 'elmo',
+					age: '7',
+					email: 'elmo@sesamestreet.com'
+				}
+			];
+			
+			$scope.showThis = true;
+			
+			vm.testing = 'Testing';
+		},
+		
+		// controllerAs: 'ddctrl',
+		
+		
+		link: function(scope, iElement, iAttribs) {
+			iElement.on('click', function() {
+				if (scope.showThis) {
+					scope.showThis = false;
+				} else scope.showThis = true;
+				
+				scope.$apply();
+			});
+			
+			iElement.on('load', function() {
+				scope.$apply();
+			});
+			
+			console.log('test');
+		}
+		// bindToController: true
+	}
+}]);
